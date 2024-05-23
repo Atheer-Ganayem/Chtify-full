117850f80c71a6e06fc466da0a86fa62aacdcdfb
Atheer-Ganayem <atheer95@gmail.com>
2024-05-23 16:46:33 +0300

    api url updaed

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     AWS: "https://chatify-project.s3.eu-central-1.amazonaws.com/",
-    API: "http://localhost:5000",
+    API: "https://chatify-api-1m63.onrender.com",
   },
   images: {
     remotePatterns: [
